e267a999c897dc8f25e2b925aedfc0753f0766ef
NTS <slweetlov4@gmail.com>
2024-10-05 14:50:26 +0700

    use Evironment Variables for db and session

--- Backend/9.4+Environment+Variables/index.js
+++ Backend/9.4+Environment+Variables/index.js
@@ -26,11 +26,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "171204",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
